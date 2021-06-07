6ae72581bdbcb29f9d69ab9803b565c5c6ed9655
Kelvin <kt16@sanger.ac.uk>
2021-06-07 23:52:55 +0100

    Update generics.R

--- R/generics.R
+++ R/generics.R
@@ -1,9 +1,20 @@
 #' @param x .gmx, .gmt, .csv, .tsv, .txt, data.frame or list object containing gene sets.
-#' @param ... Arguments passed to other methods
+#' @param ... Arguments passed to other methods.
 #
 #' @return Returns a list of gene sets.
 #'
 
 .read_geneset <- function(x, ...) {
     UseMethod(generic = "read_geneset")
+}
+
+#' @param x either a list or a BOWER class
+#' @param ... Arguments passed to other methods.
+#
+#' @return Returns a SNN geneset graph.
+#' @export
+#'
+
+snn_graph <- function(x, ...) {
+    UseMethod(generic = "snn_graph")
 }
