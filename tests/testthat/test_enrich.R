a3858889fba880773a4d5c8a837260ff38ad1eb0
Kelvin <kt16@sanger.ac.uk>
2021-07-28 08:50:18 +0100

    Update test_enrich.R

--- tests/testthat/test_enrich.R
+++ tests/testthat/test_enrich.R
@@ -1,83 +1,133 @@
+library(ktplots)
+library(ggplot2)
+data(kidneyimmune)
+seu <- kidneyimmune
+sce <- as.SingleCellExperiment(seu)
+celltypes <- levels(seu)
+gmt_file <- system.file("extdata", "h.all.v7.4.symbols.gmt", package = "bowerbird")
+bwr <- bower(gmt_file)
+bwr <- snn_graph(bwr)
+bwr <- find_clusters(bwr)
+bwr <- summarize_clusters(bwr)
+degs <- Seurat::FindAllMarkers(seu)
+degs <- split(degs, degs$cluster)
+seu$celltype2 <- as.character(seu$celltype)
+sce$celltype2 <- as.character(sce$celltype)
+
 test_that("enrich genesets deg", {
-	library(ktplots)
-	data(kidneyimmune)
-	celltypes <- levels(kidneyimmune)
-	gmt_file <- system.file("extdata", "h.all.v7.4.symbols.gmt", package = "bowerbird")
-	bwr <- bower(gmt_file)
-	bwr <- snn_graph(bwr)
-	bwr <- find_clusters(bwr)
-	bwr <- summarize_clusters(bwr)
+    # so in practice, there should be one DEG table per comparison in a list.
+    bwr <- enrich_genesets(degs, bwr, gene_symbol = 'gene', logfoldchanges = 'avg_log2FC',  pvals = 'p_val')
+    expect_visible(bwr@scores)
+    expect_type(bwr@scores, 'list')
+    expect_length(bwr@scores, 6)
+    for (ds in celltypes){
+        p <- plot_graph(bwr, colorby = ds, mode = 'gsea', gsea.slot = 'NES')
+        expect_true(is.ggplot(p))
+        expect_length(p$layers, 3)
+    }
 
-	degs <- Seurat::FindAllMarkers(kidneyimmune)
-	degs <- split(degs, degs$cluster) # so in practice, there should be one DEG table per comparison in a list.
-	bwr <- enrich_genesets(degs, bwr, gene_symbol = 'gene', logfoldchanges = 'avg_log2FC',  pvals = 'p_val')
-	
-	plot_list <- lapply(celltypes, function(ds){
-		g <- plot_graph(bwr, colorby = ds, mode = 'gsea', gsea.slot = 'NES')
-	})
-	cowplot::plot_grid(plotlist = plot_list, scale = 0.9)
+    bwr <- enrich_genesets(degs, bwr, core = TRUE, gene_symbol = 'gene', logfoldchanges = 'avg_log2FC',  pvals = 'p_val')
+    expect_visible(bwr@scores)
+    expect_type(bwr@scores, 'list')
+    expect_length(bwr@scores, 6)
 })
-
+expect_visible(bwr@scores)
 test_that("enrich genesets seurat", {
-	library(ktplots)
-	data(kidneyimmune)
-	celltypes <- levels(kidneyimmune)
-	gmt_file <- system.file("extdata", "h.all.v7.4.symbols.gmt", package = "bowerbird")
-	bwr <- bower(gmt_file)
-	bwr <- snn_graph(bwr)
-	bwr <- find_clusters(bwr)
-	bwr <- summarize_clusters(bwr)
-
-	bwr <- enrich_genesets(kidneyimmune, bwr, groupby = 'celltype', mode = 'AUCell')
-	bwr <- enrich_genesets(kidneyimmune, bwr, groupby = 'celltype', mode = 'AUCell', core = TRUE)
-	## Seurat::AddModuleScore
-	bwr <- enrich_genesets(kidneyimmune, bwr, groupby = 'celltype', mode = 'Seurat')
-	bwr <- enrich_genesets(kidneyimmune, bwr, groupby = 'celltype', mode = 'Seurat', core = TRUE)
-	## scanpy.tl.score_genes
-	bwr <- enrich_genesets(kidneyimmune, bwr, groupby = 'celltype', mode = 'scanpy')
-	bwr <- enrich_genesets(kidneyimmune, bwr, groupby = 'celltype', mode = 'scanpy', core = TRUE)
-
-	kidneyimmune$celltype2 <- as.character(kidneyimmune$celltype)
-	bwr <- enrich_genesets(kidneyimmune, bwr, groupby = 'celltype2', mode = 'AUCell')
-	bwr <- enrich_genesets(kidneyimmune, bwr, groupby = 'celltype2', mode = 'AUCell', core = TRUE)
-	## Seurat::AddModuleScore
-	bwr <- enrich_genesets(kidneyimmune, bwr, groupby = 'celltype2', mode = 'Seurat')
-	bwr <- enrich_genesets(kidneyimmune, bwr, groupby = 'celltype2', mode = 'Seurat', core = TRUE)
-	## scanpy.tl.score_genes
-	bwr <- enrich_genesets(kidneyimmune, bwr, groupby = 'celltype2', mode = 'scanpy')
-	bwr <- enrich_genesets(kidneyimmune, bwr, groupby = 'celltype2', mode = 'scanpy', core = TRUE)
+    bwr <- enrich_genesets(seu, bwr, groupby = 'celltype', mode = 'AUCell')
+    expect_visible(bwr@scores)
+    expect_is(bwr@scores, 'matrix')
+    
+    bwr <- enrich_genesets(seu, bwr, groupby = 'celltype', mode = 'AUCell', core = TRUE)
+    expect_visible(bwr@scores)
+    expect_is(bwr@scores, 'matrix')
+    
+    bwr <- enrich_genesets(seu, bwr, groupby = 'celltype', mode = 'Seurat')
+    expect_visible(bwr@scores)
+    expect_is(bwr@scores, 'matrix')
+    
+    bwr <- enrich_genesets(seu, bwr, groupby = 'celltype', mode = 'Seurat', core = TRUE)
+    expect_visible(bwr@scores)
+    expect_is(bwr@scores, 'matrix')
+    
+    bwr <- enrich_genesets(seu, bwr, groupby = 'celltype', mode = 'scanpy')
+    expect_visible(bwr@scores)
+    expect_is(bwr@scores, 'matrix')
+    
+    bwr <- enrich_genesets(seu, bwr, groupby = 'celltype', mode = 'scanpy', core = TRUE)
+    expect_visible(bwr@scores)
+    expect_is(bwr@scores, 'matrix')
+    
+    bwr <- enrich_genesets(seu, bwr, groupby = 'celltype2', mode = 'AUCell')
+    expect_visible(bwr@scores)
+    expect_is(bwr@scores, 'matrix')
+    
+    bwr <- enrich_genesets(seu, bwr, groupby = 'celltype2', mode = 'AUCell', core = TRUE)
+    expect_visible(bwr@scores)
+    expect_is(bwr@scores, 'matrix')
+    
+    bwr <- enrich_genesets(seu, bwr, groupby = 'celltype2', mode = 'Seurat')
+    expect_visible(bwr@scores)
+    expect_is(bwr@scores, 'matrix')
+    
+    bwr <- enrich_genesets(seu, bwr, groupby = 'celltype2', mode = 'Seurat', core = TRUE)
+    expect_visible(bwr@scores)
+    expect_is(bwr@scores, 'matrix')
+    
+    bwr <- enrich_genesets(seu, bwr, groupby = 'celltype2', mode = 'scanpy')
+    expect_visible(bwr@scores)
+    expect_is(bwr@scores, 'matrix')
+    
+    bwr <- enrich_genesets(seu, bwr, groupby = 'celltype2', mode = 'scanpy', core = TRUE)
+    expect_visible(bwr@scores)
+    
 })
 
 test_that("enrich genesets sce", {
-	library(ktplots)
-	data(kidneyimmune)
-	celltypes <- levels(kidneyimmune)
-
-	scex <- as.SingleCellExperiment(kidneyimmune)
-
-	gmt_file <- system.file("extdata", "h.all.v7.4.symbols.gmt", package = "bowerbird")
-	bwr <- bower(gmt_file)
-	bwr <- snn_graph(bwr)
-	bwr <- find_clusters(bwr)
-	bwr <- summarize_clusters(bwr)
-
-	bwr <- enrich_genesets(scex, bwr, groupby = 'celltype', mode = 'AUCell')
-	bwr <- enrich_genesets(scex, bwr, groupby = 'celltype', mode = 'AUCell', core = TRUE)
-	## Seurat::AddModuleScore
-	bwr <- enrich_genesets(scex, bwr, groupby = 'celltype', mode = 'Seurat')
-	bwr <- enrich_genesets(scex, bwr, groupby = 'celltype', mode = 'Seurat', core = TRUE)
-	## scanpy.tl.score_genes
-	bwr <- enrich_genesets(scex, bwr, groupby = 'celltype', mode = 'scanpy')
-	bwr <- enrich_genesets(scex, bwr, groupby = 'celltype', mode = 'scanpy', core = TRUE)
-
-	scex$celltype2 <- as.character(scex$celltype)
-	bwr <- enrich_genesets(scex, bwr, groupby = 'celltype2', mode = 'AUCell')
-	bwr <- enrich_genesets(scex, bwr, groupby = 'celltype2', mode = 'AUCell', core = TRUE)
-	## Seurat::AddModuleScore
-	bwr <- enrich_genesets(scex, bwr, groupby = 'celltype2', mode = 'Seurat')
-	bwr <- enrich_genesets(scex, bwr, groupby = 'celltype2', mode = 'Seurat', core = TRUE)
-	## scanpy.tl.score_genes
-	bwr <- enrich_genesets(scex, bwr, groupby = 'celltype2', mode = 'scanpy')
-	bwr <- enrich_genesets(scex, bwr, groupby = 'celltype2', mode = 'scanpy', core = TRUE)
-
+    bwr <- enrich_genesets(sce, bwr, groupby = 'celltype', mode = 'AUCell')
+    expect_visible(bwr@scores)
+    expect_is(bwr@scores, 'matrix')
+    
+    bwr <- enrich_genesets(sce, bwr, groupby = 'celltype', mode = 'AUCell', core = TRUE)
+    expect_visible(bwr@scores)
+    expect_is(bwr@scores, 'matrix')
+    
+    bwr <- enrich_genesets(sce, bwr, groupby = 'celltype', mode = 'Seurat')
+    expect_visible(bwr@scores)
+    expect_is(bwr@scores, 'matrix')
+    
+    bwr <- enrich_genesets(sce, bwr, groupby = 'celltype', mode = 'Seurat', core = TRUE)
+    expect_visible(bwr@scores)
+    expect_is(bwr@scores, 'matrix')
+    
+    bwr <- enrich_genesets(sce, bwr, groupby = 'celltype', mode = 'scanpy')
+    expect_visible(bwr@scores)
+    expect_is(bwr@scores, 'matrix')
+    
+    bwr <- enrich_genesets(sce, bwr, groupby = 'celltype', mode = 'scanpy', core = TRUE)
+    expect_visible(bwr@scores)
+    expect_is(bwr@scores, 'matrix')
+    
+    bwr <- enrich_genesets(sce, bwr, groupby = 'celltype2', mode = 'AUCell')
+    expect_visible(bwr@scores)
+    expect_is(bwr@scores, 'matrix')
+    
+    bwr <- enrich_genesets(sce, bwr, groupby = 'celltype2', mode = 'AUCell', core = TRUE)
+    expect_visible(bwr@scores)
+    expect_is(bwr@scores, 'matrix')
+    
+    bwr <- enrich_genesets(sce, bwr, groupby = 'celltype2', mode = 'Seurat')
+    expect_visible(bwr@scores)
+    expect_is(bwr@scores, 'matrix')
+    
+    bwr <- enrich_genesets(sce, bwr, groupby = 'celltype2', mode = 'Seurat', core = TRUE)
+    expect_visible(bwr@scores)
+    expect_is(bwr@scores, 'matrix')
+    
+    bwr <- enrich_genesets(sce, bwr, groupby = 'celltype2', mode = 'scanpy')
+    expect_visible(bwr@scores)
+    expect_is(bwr@scores, 'matrix')
+    
+    bwr <- enrich_genesets(sce, bwr, groupby = 'celltype2', mode = 'scanpy', core = TRUE)
+    expect_visible(bwr@scores)
+    
 })
